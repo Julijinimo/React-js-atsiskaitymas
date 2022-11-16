63d915abfab0e76f479940e4409893ec8d898bb2
Julijinimo <ownflame@gmail.com>
2022-11-16 16:36:37 +0200

    updatinta inf, sukurti componentai

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BASE_URL } from './Utils/constants';
+
 import './App.css';
 
+
 function App() {
+  const [registerError, setRegisterError] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    fetch(`${BASE_URL}/auth/register`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: 'test123@gmail.com',
+        password: 'test123'
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if (data.err) {
+        setRegisterError(data.err);
+      } else {
+        localStorage.setItem("token", data.token) // login komponente
+
+
+        //kodas zemiau rasysi i home komponenta
+        localStorage.getItem("token") // sita naudosiu kai noresiu gauti skills, einant per fecth
+
+        fetch(BASE_URL + "/content/skills", {
+          headers: {
+            Authorization: "Bearer" + token
+          }
+        })
+      }
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. 
-          Ar tikrai?
-          A?
-          A?
-        </p>
-      </header>
+      {registerError && <h3>{registerError}</h3>}
+      <form onSubmit={handleLogin}>
+        <input type="email" placeholder='Email'/>
+        <input type="password" placeholder='Password'/>
+        <button>Register</button>
+      </form>
     </div>
   );
 }
